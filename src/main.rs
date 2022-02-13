c316452a08ccefa0a18628b39723e399c6b390ea
Benjamin Hirsch <mail@benjaminhirsch.net>
2022-02-13 21:52:47 +0100

    Working url parser with respecting filters and duplicates

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,42 @@
-use std::borrow::{Borrow, Cow};
-use std::convert::Infallible;
 #[allow(unused,unused_imports,dead_code)]
 use std::env;
-use std::os::linux::raw::stat;
-//use parse_hyperlinks::iterator::Hyperlink;
-//use parse_hyperlinks::parser::html::{html_text2dest, html_text2dest_link};
-use reqwest::StatusCode;
 use select::document::Document;
 use select::predicate::Name;
 
 
 #[derive(Debug)]
 struct UrlsToParse {
-    pub urls: Vec<Url>
+    pub urls: Vec<String>
 }
 
 #[derive(Debug)]
 struct ParsedUrls {
-    urls: Vec<Url>
+    urls: Vec<ParsedUrl>
 }
 
 #[derive(Debug)]
 struct Url {
     url: String,
-    status_code: u8,
+    status_code: u16,
+    body: String,
 }
 
-impl From<String> for Url {
-    fn from(url: String) -> Self {
+impl Url {
+    pub fn create(url: String, status_code: u16, body: String) -> Self {
         Url {
             url,
-            status_code: 0
+            status_code,
+            body
         }
     }
 }
+
+#[derive(Debug)]
+struct ParsedUrl {
+    url: String,
+    status_code: u16,
+}
+
 
 impl UrlsToParse {
     pub fn create() -> Self {
@@ -42,14 +45,13 @@ impl UrlsToParse {
         }
     }
 
-    pub fn add(&mut self, url: Url) -> &Self {
+    pub fn add(&mut self, url: String) -> &Self {
         self.urls.push(url);
         self
     }
 
-    pub fn remove(&mut self, index: usize) -> &Self {
-        self.urls.remove(index);
-        self
+    pub fn has(&mut self, url: &String) -> bool {
+        self.urls.iter().any(|u| u == url)
     }
 }
 
@@ -60,9 +62,22 @@ impl ParsedUrls {
         }
     }
 
-    pub fn add(&mut self, url: Url) -> &Self {
+    pub fn add(&mut self, url: ParsedUrl) -> &Self {
         self.urls.push(url);
         self
+    }
+
+    pub fn has(&mut self, url: &String) -> bool {
+        self.urls.iter().any(|u| u.url == url.to_string())
+    }
+}
+
+impl From<&Url> for ParsedUrl {
+    fn from(url: &Url) -> Self {
+        ParsedUrl {
+            url: url.url.to_string(),
+            status_code: url.status_code
+        }
     }
 }
 
@@ -73,38 +88,41 @@ async fn main()  -> Result<(), Box<dyn std::error::Error>> {
     println!("Starting to parse: {}", args[1]);
 
     let mut sites = UrlsToParse::create();
-    sites.add(Url::from(args[1].clone()));
+    let mut sites_processed = ParsedUrls::create();
+
+    sites.add(args[1].to_string());
+    let domain = args[1].clone();
 
     while sites.urls.len() > 0 {
         let current_url = sites.urls.pop();
         match current_url {
-            Some(ref actual_url) => {
-                println!("Parsing: {}", actual_url.url);
-                let response = reqwest::get(&actual_url.url).await?;
+            Some(actual_url) => {
+                //println!("Parsing: {}", actual_url);
+                let response = reqwest::get(&actual_url).await?;
+                let is_success = response.status().is_success();
+                let status_code = response.status().as_u16();
+                let body_text = response.text().await?;
 
-                let status = response.status().as_u16();
+                let url = &Url::create(
+                    actual_url,
+                    status_code,
+                    body_text
+                );
+                sites_processed.add(ParsedUrl::from(url));
+                println!("Processed  {} of {} sites", sites_processed.urls.len(), sites.urls.len());
 
-                if status >= 200 && status < 300 {
-                    //println!("{:?}", response.text().await?);
-                    let body_text = response.text().await?;
-
-                    match Document::try_from(body_text.as_str()) {
+                if is_success {
+                    match Document::try_from(url.body.as_str()) {
                         Ok(document) => {
                             document.find(Name("a"))
-                                .filter(|n| {
-                                    // Remove empty or self referenced node
-                                    if vec!["#", ""].iter().any(|&nv| nv == n.attr("href").unwrap()) {
-                                        return false
-                                    }
-                                    true
-                                })
+                                //.filter(|n| !vec!["#", ""].iter().any(|&nv| nv == n.attr("href")))
                                 .filter_map(|n| n.attr("href"))
-                                //.for_each(|x| println!("{}", x))
                                 .for_each(|x| {
-
                                     // We need to add checks here, to prevent parsing errors because of invalid urls
-
-                                    sites.add(Url::from(x.to_string()));
+                                    let normalized_url = normalize_url(x, &domain);
+                                    if is_valid_url(&normalized_url, &domain) && (!sites_processed.has(&normalized_url) && !sites.has(&normalized_url)) {
+                                        sites.add(normalized_url);
+                                    }
                                 })
                         }
                         Err(_) => {
@@ -133,13 +151,33 @@ async fn main()  -> Result<(), Box<dyn std::error::Error>> {
 
                     //}
                 }
-
-
             },
             None => println!("{}", "Something went wrong, execution is continuing...")
         }
     }
 
-    //println!("{:#?}", sites);
+    println!("Finished, checked {} sites", sites_processed.urls.len());
     Ok(())
+}
+
+fn is_valid_url(url: &str, domain: &String) -> bool {
+    url.starts_with(domain) && !url.starts_with("#") && !url.starts_with("tel")
+}
+
+fn normalize_url(url: &str, domain: &String) -> String {
+    match url.chars().nth(0) {
+        Some(string) => {
+            return if string.to_string() == "/" {
+                domain.to_string() + &url[1..url.len()]
+            } else {
+                url.to_string()
+            }
+        },
+        None => url.to_string()
+    }
+    //if url.chars().nth(0).unwrap().to_string() == "/" {
+        //return domain.to_string() + &url[1..url.len()]
+    //}
+
+
 }
